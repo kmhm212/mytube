7af3ce59171b624135f81451ab7da75352d930ea
yuta-nakaa <nakshimashelter55@gmail.com>
2021-05-20 23:35:36 +0900

    動画一覧の表示

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<?php include_once __DIR__ . '/_head.htm' ?>
+
+<body>
+    <div class="wrapper">
+        <h1 class="title"><a href="index.php">MyTube</a></h1>
+        <a href="new.php" class="btn new-btn">NEW</a>
+        <section class="movie-list">
+            <div class="movie">
+                <p class="movie-title"><a href="show.php">
+                        Ed Sheeran - Shape of You</a></p>
+                <div class="youtube">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/JGwWNGJdvx8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="movie">
+                <p class="movie-title"><a href="show.php">
+                        Billie Eilish - bad guy</a></p>
+                <div class="youtube">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/DyDfgMOUjCI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="movie">
+                <p class="movie-title"><a href="show.php">
+                        BTS (방탄소년단) 'Dynamite' Official MV</a></p>
+                <div class="youtube">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/gdZLi9oWNZg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="movie">
+                <p class="movie-title"><a href="show.php">
+                        やる気を全開にさせる最強洋楽メドレー2021</a></p>
+                <div class="youtube">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/GmOO1wSGwhY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="movie">
+                <p class="movie-title"><a href="show.php">
+                        Hip Hop Mix 2020</a></p>
+                <div class="youtube">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/bImx3tpGR5w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="movie">
+                <p class="movie-title"><a href="show.php">
+                        Pop rnb chill mix</a></p>
+                <div class="youtube">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/mV8N-V8F9j8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+
+</html>
